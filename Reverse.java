b147aa5c8f6f5919dda37a4ab49ea29945aa7619
Joey <118591147+rocco949@users.noreply.github.com>
2023-01-25 22:27:04 -0800

    moved stuff around for a pretty file

--- Reverse.java
+++ Reverse.java
@@ -1,41 +1,34 @@
 import java.util.Scanner;
 
-
 public class Reverse {
-    
+    final static Scanner obj_sc = new Scanner(System.in);
+    final static Reverse obj_rv = new Reverse();
+
+    static String word;
 
     public static void reverse(String[] word, int length) {
-         Reverse obj_rv = new Reverse();
         for (int i = length; i >= 0; --i) {
-            
-            System.out.print(word[i]);
+            System.out.print(word[i]);  
         }
-        System.out.println();
-         obj_rv.ending();
+        obj_rv.ending();
     }
 
     public void starter() {
-        Scanner obj_sc = new Scanner(System.in);
-
         System.out.println("Enter a word: ");
-        String word = obj_sc.nextLine();
-        
+        word = obj_sc.nextLine();
         reverse(word.split(""), word.length() - 1);
     }
     
     public Boolean doItAgain() {
-        Scanner obj_sc = new Scanner(System.in);
-
         System.out.println("You wish to do it again (Y,n)");
         String answer = obj_sc.nextLine();
-
-        Boolean returnValue = (answer.equalsIgnoreCase("Y"))?true : false;
+        Boolean returnValue = 
+        (answer.equalsIgnoreCase("Y"))?true : false;
 
         return returnValue;
     }
 
     public void ending() {
-        Reverse obj_rv = new Reverse();
         if(obj_rv.doItAgain()) {
             System.out.println("");
             obj_rv.starter();
@@ -45,15 +38,8 @@ public class Reverse {
     }
 
     public static void main(String[] args){
-        Reverse obj_rv = new Reverse();
-
         obj_rv.starter();
-        
-
-
-        
-}
-
+    }
         }
 
         
